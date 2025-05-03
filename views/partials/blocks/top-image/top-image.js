fff35d97a82e641f28a132ad5b1635972ef83dee
kuh1012 <inner.25.peace@gmail.com>
2025-05-02 20:21:30 -0700

    wip: fixing the window height where it fits with the address bar

--- views/partials/blocks/top-image/top-image.js
+++ views/partials/blocks/top-image/top-image.js
@@ -1,21 +1,9 @@
-// function setInnerHeight() {
-//     console.log('ss', `${window.innerHeight}`);
-//     document.documentElement.style.setProperty('--slider-container', `${window.innerHeight / 2 - 32.5 - 5}px`);
-//     document.documentElement.style.setProperty('--text-block', `${window.innerHeight / 2 - 32.5 - 2.5}px`);
-// }
+function setInnerHeight() {
+    console.log('ss', `${window.innerHeight}`);
+    document.documentElement.style.setProperty('--slider-container', `${window.innerHeight / 2 - 32.5 - 5}px`);
+    document.documentElement.style.setProperty('--text-block', `${window.innerHeight / 2 - 32.5 - 2.5}px`);
+}
 
-// window.addEventListener('resize', setInnerHeight);
+window.addEventListener('load', setInnerHeight);
 
-let initialHeight = window.innerHeight;
 
-window.addEventListener('resize', () => {
-    const currentHeight = window.innerHeight;
-
-    if (currentHeight > initialHeight) {
-        console.log("address bar likely hidden");
-    } else if (currentHeight < initialHeight) {
-        console.log("address bar likely shown");
-    }
-
-    initialHeight = currentHeight;
-})
